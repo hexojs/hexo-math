151b3a78c993b59d5aed076d3b24f8d3dd71018d
segayuu <segayuu@gmail.com>
2019-04-21 13:12:44 +0900

    Fix style lint

--- src/filter/post.mjs
+++ src/filter/post.mjs
@@ -1,33 +1,32 @@
-import { BLOCK_MATH_RENDER_REGEX, INLINE_MATH_RENDER_REGEX } from '../consts'
-import { AllHtmlEntities } from "html-entities";
-import katex from 'katex'
-const entities = new AllHtmlEntities()
+import { BLOCK_MATH_RENDER_REGEX, INLINE_MATH_RENDER_REGEX } from '../consts';
+import { AllHtmlEntities } from 'html-entities';
+import katex from 'katex';
+const entities = new AllHtmlEntities();
 
 export default class Post {
-  constructor (hexo, opts) {
-    this.hexo = hexo
-    this.opts = opts
+  constructor(hexo, opts) {
+    this.hexo = hexo;
+    this.opts = opts;
   }
-  register () {
-    if (this.opts.engine !== 'katex') return
-    const { filter } = this.hexo.extend
-    filter.register('before_post_render', this._transform.bind(this))
+  register() {
+    if (this.opts.engine !== 'katex') return;
+    const { filter } = this.hexo.extend;
+    filter.register('before_post_render', this._transform.bind(this));
   }
-  _transform (data) {
-    data.content = data.content.replace(BLOCK_MATH_RENDER_REGEX, (m, math) => this._render(m, math, true))
-    data.content = data.content.replace(INLINE_MATH_RENDER_REGEX, (m, math) => this._render(m, math, false))
-    return data
+  _transform(data) {
+    data.content = data.content.replace(BLOCK_MATH_RENDER_REGEX, (m, math) => this._render(m, math, true));
+    data.content = data.content.replace(INLINE_MATH_RENDER_REGEX, (m, math) => this._render(m, math, false));
+    return data;
   }
-  _render (match, math, isBlock) {
-    const opts = Object.assign({}, this.opts.katex.config, { displayMode: isBlock })
+  _render(match, math, isBlock) {
+    const opts = Object.assign({}, this.opts.katex.config, { displayMode: isBlock });
 
     try {
-      return katex.renderToString(entities.decode(math), opts)
-
+      return katex.renderToString(entities.decode(math), opts);
     } catch (e) {
-      this.hexo.log.error(e)
+      this.hexo.log.error(e);
     }
 
-    return match
+    return match;
   }
 }

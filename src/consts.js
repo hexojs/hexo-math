38101f96a3fb3079dc5d3bd5286fac077b1ce8ba
Thomas Piart <piartt@gmail.com>
2019-08-14 19:11:07 +0200

    fix conflicts

--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,9 @@
-export const INLINE_MATH_REGEX = /\$[\s\S]+?\$/;
-export const BLOCK_MATH_REGEX = /^\s*\$\$[\s\S]*\$\$\s*$/m;
-export const INLINE_MATH_RENDER_REGEX = /\$([^\n]+?)\$/g;
-export const BLOCK_MATH_RENDER_REGEX = /\$\$([\s\S]+?)\$\$/g;
+'use strict';
+module.exports.INLINE_MATH_REGEX = /\$[\s\S]+?\$/;
+module.exports.BLOCK_MATH_REGEX = /^\s*\$\$[\s\S]*\$\$\s*$/m;
+module.exports.INLINE_MATH_RENDER_REGEX = /\$([^\n]+?)\$/g;
+module.exports.BLOCK_MATH_RENDER_REGEX = /\$\$([\s\S]+?)\$\$/g;
 
-export const MATH_MARKER = '<!-- Has MathJax -->';
-export const KATEX_INLINE_MARKER = '<span class="katex">';
-export const KATEX_BLOCK_MARKER = '<span class="katex-display">';
+module.exports.MATH_MARKER = '<!-- Has MathJax -->';
+module.exports.KATEX_INLINE_MARKER = '<span class="katex">';
+module.exports.KATEX_BLOCK_MARKER = '<span class="katex-display">';
--- src/filter/post.js
+++ src/filter/post.js
@@ -1,34 +1,34 @@
-import { BLOCK_MATH_RENDER_REGEX, INLINE_MATH_RENDER_REGEX } from '../consts'
-import { AllHtmlEntities } from "html-entities";
-import katex from 'katex'
-import _ from 'underscore'
-const entities = new AllHtmlEntities()
+const { BLOCK_MATH_RENDER_REGEX, INLINE_MATH_RENDER_REGEX } = require('../consts');
+const { AllHtmlEntities } = require('html-entities');
+const katex = require('katex');
+const _ = require('underscore');
+const entities = new AllHtmlEntities();
 
-export default class Post {
-  constructor (hexo, opts) {
-    this.hexo = hexo
-    this.opts = opts
+module.exports = class Post {
+  constructor(hexo, opts) {
+    this.hexo = hexo;
+    this.opts = opts;
   }
-  register () {
-    if (this.opts.engine !== 'katex') return
-    const { filter } = this.hexo.extend
-    filter.register('before_post_render', this._transform.bind(this))
+  register() {
+    if (this.opts.engine !== 'katex') return;
+    const { filter } = this.hexo.extend;
+    filter.register('before_post_render', this._transform.bind(this));
   }
-  _transform (data) {
-    data.content = data.content.replace(BLOCK_MATH_RENDER_REGEX, (m, math) => this._render(m, math, true))
-    data.content = data.content.replace(INLINE_MATH_RENDER_REGEX, (m, math) => this._render(m, math, false))
-    return data
+  _transform(data) {
+    data.content = data.content.replace(BLOCK_MATH_RENDER_REGEX, (m, math) => this._render(m, math, true));
+    data.content = data.content.replace(INLINE_MATH_RENDER_REGEX, (m, math) => this._render(m, math, false));
+    return data;
   }
-  _render (match, math, isBlock) {
-    const opts = _.extend({}, this.opts.katex.config, { displayMode: isBlock })
+  _render(match, math, isBlock) {
+    const opts = _.extend({}, this.opts.katex.config, { displayMode: isBlock });
 
     try {
-      return katex.renderToString(entities.decode(math), opts)
+      return katex.renderToString(entities.decode(math), opts);
 
     } catch (e) {
-      this.hexo.log.error(e)
+      this.hexo.log.error(e);
     }
 
-    return match
+    return match;
   }
-}
+};

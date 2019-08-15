234cebcfd43b27ab8f0d2140faedb8ddc532fa09
Thomas <tomap@users.noreply.github.com>
2019-08-15 02:41:06 +0200

    Fix build by switching to js (#95)
    * Fix build by switching to js
    + align katex versions
    + update eslint to latest
    
    * fix readme
    
    * configure appveyor
    
    * fix conflicts
    
    * fix line feed

--- src/option.js
+++ src/option.js
@@ -19,8 +19,8 @@ const DEFAULT_OPTS = module.exports.DEFAULT_OPTS = {
     }
   },
   katex: {
-    css: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css',
-    js: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.js',
+    css: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css',
+    js: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js',
     config: {
       throwOnError: false,
       errorColor: '#cc0000'

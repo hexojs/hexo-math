c9a817aa52324f65386cbad4133e377038aca292
MDLeom <43627182+curbengh@users.noreply.github.com>
2020-07-26 13:25:25 +0100

    feat: reimplementation
    - Support arguments in tag plugins as renderer options
    - Support front-matter as renderer options
    - Support _config.yml as renderer options
    - Requires Node 10.13+, drop Node 8
    - chore(deps): update katex from 0.6.0 to 0.12.0
    - chore(deps): add mathjax
    - chore(deps): update hexo-util from 1.0.0 to 2.2.0
    - chore(dev-deps): update eslint from 6.1.0 to 7.5.0
    - chore(dev-deps): update eslint-config-hexo from 3.0.0 to 4.1.0
    - chore(deps): update mocha from 6.1.2 to 8.0.1
    - chore(deps): remove html-entities
    - chore(dev-deps): remove chai-as-promised, cpx, hexo-fs, npm-run-all & rimraf
    - ci(travis): add Windows
    - ci: add GitHub Actions

--- src/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const MathJax = require('./main');
-
-const math = hexo.math = new MathJax(hexo);
-math.register();
---
+++ test/index.js
@@ -0,0 +1,194 @@
+'use strict';
+
+require('chai').should();
+
+const { deepMerge, url_for } = require('hexo-util');
+const Hexo = require('hexo');
+const hexo = new Hexo(__dirname);
+const { renderToString: renderKatex } = require('katex');
+
+describe('hexo-math', () => {
+  const defaultCfg = deepMerge(hexo.config, {
+    math: {
+      katex: {
+        enable: true,
+        css: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
+        options: {
+          throwOnError: false
+        }
+      },
+      mathjax: {
+        enable: true,
+        css: 'https://cdn.jsdelivr.net/gh/hexojs/hexo-math/master/dist/style.css',
+        options: {
+          conversion: {
+            display: false
+          },
+          svg: {},
+          tex: {}
+        }
+      }
+    }
+  });
+  let args = [];
+
+  beforeEach(() => {
+    hexo.config = deepMerge(hexo.config, defaultCfg);
+    args = [];
+  });
+
+  describe('tag', () => {
+    describe('katex', () => {
+      const k = require('../lib/katex')(hexo).bind({});
+      const content = 'c = \\pm\\sqrt{a^2 + b^2}';
+
+      it('default', () => {
+        const output = k(args, content);
+        const expected = renderKatex(content, hexo.config.math.katex.options);
+
+        output.should.eql(expected);
+      });
+
+      it('config', () => {
+        hexo.config.math.katex.options.output = 'html';
+        const output = k(args, content);
+        const expected = renderKatex(content, hexo.config.math.katex.options);
+
+        output.should.eql(expected);
+      });
+
+      it('args', () => {
+        const options = { output: 'mathml' };
+        args.push(JSON.stringify(options));
+        const output = k(args, content);
+        const expected = renderKatex(content, options);
+
+        output.should.eql(expected);
+      });
+
+      it('args should override config', () => {
+        hexo.config.math.katex.options.output = 'html';
+
+        const options = { output: 'mathml' };
+        args.push(JSON.stringify(options));
+        const output = k(args, content);
+        const expected = renderKatex(content, options);
+
+        output.should.eql(expected);
+      });
+
+      it('disable in front-matter', () => {
+        const fm = { katex: false };
+        const k = require('../lib/katex')(hexo).bind(fm);
+
+        const output = k(args, content);
+        output.should.eql(content);
+      });
+
+      it('args should override front-matter', () => {
+        const fm = { katex: { output: 'html' } };
+        const k = require('../lib/katex')(hexo).bind(fm);
+        const options = { output: 'mathml' };
+        args.push(JSON.stringify(options));
+        const output = k(args, content);
+        const expected = renderKatex(content, options);
+
+        output.should.eql(expected);
+      });
+    });
+
+    describe('mathjax', () => {
+      const m = require('../lib/mathjax')(hexo).bind({});
+      const content = '\\frac{1}{x^2-1}';
+      const displayFalse = '<mjx-container class="MathJax" jax="SVG">';
+      const displayTrue = '<mjx-container class="MathJax" jax="SVG" display="true">';
+
+      it('default', async () => {
+        const output = await m(args, content);
+
+        output.startsWith(displayFalse).should.eql(true);
+      });
+
+      it('config', async () => {
+        hexo.config.math.mathjax.options.conversion.display = true;
+        const output = await m(args, content);
+
+        output.startsWith(displayTrue).should.eql(true);
+      });
+
+      it('args', async () => {
+        const options = { conversion: { display: true } };
+        args.push(JSON.stringify(options));
+        const output = await m(args, content);
+
+        output.startsWith(displayTrue).should.eql(true);
+      });
+
+      it('args should override config', async () => {
+        hexo.config.math.mathjax.options.conversion.display = false;
+
+        const options = { conversion: { display: true } };
+        args.push(JSON.stringify(options));
+        const output = await m(args, content);
+
+        output.startsWith(displayTrue).should.eql(true);
+      });
+
+      it('disable in front-matter', async () => {
+        const fm = { mathjax: false };
+        const m = require('../lib/mathjax')(hexo).bind(fm);
+
+        const output = await m(args, content);
+        output.should.eql(content);
+      });
+
+      it('args should override front-matter', async () => {
+        const fm = { mathjax: { conversion: { display: false } } };
+        const m = require('../lib/mathjax')(hexo).bind(fm);
+        const options = { conversion: { display: true } };
+        args.push(JSON.stringify(options));
+        const output = await m(args, content);
+
+        output.startsWith(displayTrue).should.eql(true);
+      });
+    });
+  });
+
+  describe('inject', () => {
+    const css = path => {
+      return `<link rel="stylesheet" href="${url_for.call(hexo, path)}">\n`;
+    };
+
+    describe('katex', () => {
+      const k = require('../lib/inject/katex')(hexo);
+
+      it('default', () => {
+        const output = k();
+        output.should.eql(css(hexo.config.math.katex.css));
+      });
+
+      it('custom path', () => {
+        const path = '/foo/bar.css';
+        hexo.config.math.katex.css = path;
+        const output = k();
+        output.should.eql(css(path));
+      });
+    });
+
+    describe('mathjax', () => {
+      const m = require('../lib/inject/mathjax')(hexo);
+
+      it('default', () => {
+        const output = m();
+        output.should.eql(css(hexo.config.math.mathjax.css));
+      });
+
+      it('custom path', () => {
+        const path = '/foo/bar.css';
+        hexo.config.math.mathjax.css = path;
+        const output = m();
+        output.should.eql(css(path));
+      });
+    });
+  });
+});
